aa7772c9efb0a78a43e6f2050677d16d64805aac
Carlos Teles <carlosgteles64@gmail.com>
2020-07-10 18:42:36 -0300

    More funcionalities added

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,19 +1,32 @@
+const fontName = document.getElementById('fontFamily')
+const fontSize = document.getElementById('fontSize')
 var editorWindow = document.getElementById('textEditor').contentDocument
+
+function fetchAllSizes () {
+  for (i = 1; i < 7; i++) {
+    fontSize.innerHTML += `<option value="${i}">${i}</option>`
+  }
+}
 
 function onLoad () {
   editorWindow.designMode = 'On'
+  fetchAllSizes()
 }
 
 function doRichTextCommand (cmd, arg) {
-  
-  if(cmd === 'createLink') {
-    console.log('cheguei aqui')
-    arg = prompt('http:// link aqui...')
-  }
-
   editorWindow.execCommand(cmd, false, arg)
-  
 }
 
-
 window.addEventListener('load', onLoad)
+
+fontSize.addEventListener('change', (event) => {
+  const selectedSize = event.target.value
+  console.log(selectedSize)
+  doRichTextCommand('fontSize', selectedSize)
+})
+
+fontName.addEventListener('change', (event) => {
+  const selectedFont = event.target.value
+  console.log(selectedFont)
+  doRichTextCommand('fontName', selectedFont)
+})
